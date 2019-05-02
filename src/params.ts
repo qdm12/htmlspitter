56436038eee030a3eb7afd6e920396e1208453f8
Quentin McGaw <quentin.mcgaw@gmail.com>
2019-05-02 11:42:07 +0200

    Env variables renaming

--- src/params.ts
+++ src/params.ts
@@ -14,12 +14,12 @@ export class Params {
         debugLog.params("reading parameters");
         this.port = Number(env.PORT) || 8000;
         this.executablePath = env.CHROME_BIN || "Puppeteer-bundled";
-        this.maxPages = Number(env.MAXPAGES) || 10;
-        this.maxHits = Number(env.MAXHITS) || 300;
-        this.maxAgeUnused = Number(env.MAXAGEUNUSED) || 60;
-        this.maxBrowsers = Number(env.MAXBROWSERS) || 10;
-        this.maxCacheSize = Number(env.MAXCACHESIZE) || 10000000;
-        this.maxQueueSize = Number(env.MAXQUEUESIZE) || 100;
+        this.maxPages = Number(env.MAX_PAGES) || 10;
+        this.maxHits = Number(env.MAX_HITS) || 300;
+        this.maxAgeUnused = Number(env.MAX_AGE_UNUSED) || 60;
+        this.maxBrowsers = Number(env.MAX_BROWSERS) || 10;
+        this.maxCacheSize = Number(env.MAX_CACHE_SIZE) || 10000000;
+        this.maxQueueSize = Number(env.MAX_QUEUE_SIZE) || 100;
         this.log = env.LOG || "normal";
     }
     toString() {
