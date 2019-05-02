a04152f6cc9a34abd4571d673183c977d7a67675
Quentin McGaw <quentin.mcgaw@gmail.com>
2019-05-02 10:16:09 +0200

    Added jest and unit test for queue

---
+++ src/queue.test.ts
@@ -0,0 +1,103 @@
+import { Queue } from "./queue";
+
+describe("constructor", () => {
+    it("creates a queue of length 10", () => {
+        const q = new Queue(10);
+        expect(q.ids.length).toBe(0);
+        expect(q.idsInQueue.size).toBe(0);
+        expect(q.maxSize).toBe(10);
+    });
+    it("creates a queue of unlimited length", () => {
+        const q = new Queue(-1);
+        expect(q.ids.length).toBe(0);
+        expect(q.idsInQueue.size).toBe(0);
+        expect(q.maxSize).toBe(-1);
+    });
+});
+
+describe("getNextID", () => {
+    it("gets first ID", () => {
+        const q = new Queue(10);
+        const id = q.getNextID();
+        expect(id).toBe(0);
+    });
+    it("gets first and second ID", () => {
+        const q = new Queue(10);
+        const id1 = q.getNextID();
+        const id2 = q.getNextID();
+        expect(id1).toBe(0);
+        expect(id2).toBe(1);
+    });
+    it("gets first and second ID", () => {
+        const q = new Queue(10);
+        const id1 = q.getNextID();
+        const id2 = q.getNextID();
+        expect(id1).toBe(0);
+        expect(id2).toBe(1);
+    });
+    it("gets ID 1 in the middle", () => {
+        const q = new Queue(10);
+        q.push();
+        q.push();
+        q.push();
+        q.shift();
+        q.shift();
+        q.push();
+        const id = q.getNextID();
+        expect(id).toBe(1);
+    });
+});
+
+describe("push", () => {
+    it("once", () => {
+        const q = new Queue(10);
+        q.push();
+        expect(q.ids.length).toBe(1);
+        expect(q.idsInQueue.size).toBe(1);
+    });
+    it("multiple times", () => {
+        const q = new Queue(10);
+        q.push();
+        q.push();
+        expect(q.ids.length).toBe(2);
+        expect(q.idsInQueue.size).toBe(2);
+    });
+    it("too many times", () => {
+        const q = new Queue(2);
+        q.push();
+        q.push();
+        const f = () => q.push();
+        expect(f).toThrowError("queue reached its maximum size")
+        expect(q.ids.length).toBe(2);
+        expect(q.idsInQueue.size).toBe(2);
+    });
+});
+
+describe("shift", () => {
+    it("once for queue of size 2", () => {
+        const q = new Queue(10);
+        q.push();
+        q.push();
+        q.shift();
+        expect(q.ids.length).toBe(1);
+        expect(q.idsInQueue.size).toBe(1);
+    });
+    it("all in queue", () => {
+        const q = new Queue(10);
+        q.push();
+        q.push();
+        q.shift();
+        q.shift();
+        expect(q.ids.length).toBe(0);
+        expect(q.idsInQueue.size).toBe(0);
+    });
+    it("too many times", () => {
+        const q = new Queue(10);
+        q.push();
+        q.shift();
+        const f = () => q.shift();
+        expect(f).toThrowError("cannot shift because queue is empty")
+        expect(q.ids.length).toBe(0);
+        expect(q.idsInQueue.size).toBe(0);
+    });
+});
--- src/queue.ts
+++ src/queue.ts
@@ -1,8 +1,8 @@
 export class Queue {
-    ids:number[];
-    idsInQueue:Set<number>;
-    maxSize:number;
-    constructor(maxSize:number) {
+    ids: number[];
+    idsInQueue: Set<number>;
+    maxSize: number;
+    constructor(maxSize: number) { // -1 for unlimited queue
         this.maxSize = maxSize;
         this.ids = [];
         this.idsInQueue = new Set();
@@ -14,26 +14,28 @@ export class Queue {
         }
         const id = this.getNextID();
         this.ids.push(id);
-        this.idsInQueue.add(id);
         return id;
     }
     getNextID() {
-        for(let nextID = 0; nextID < this.idsInQueue.size; nextID++) {
+        for (let nextID = 0; nextID < this.idsInQueue.size; nextID++) {
             if (!this.idsInQueue.has(nextID)) {
+                this.idsInQueue.add(nextID);
                 return nextID;
             }
         }
         // Need to create a new index
-        return this.idsInQueue.size;
+        const nextID = this.idsInQueue.size
+        this.idsInQueue.add(nextID);
+        return nextID;
     }
     shift() {
         const idRemoved = this.ids.shift();
         if (idRemoved === undefined) {
-            throw Error("ID shifted out from queue is undefined");
+            throw Error("cannot shift because queue is empty");
         }
         this.idsInQueue.delete(idRemoved);
     }
-    isFirst(id:number) {
+    isFirst(id: number) {
         return id === this.ids[0];
     }
 }
