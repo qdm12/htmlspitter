4e85c62b22b1d9f9dd894383887e682c5a0f8ccd
Quentin McGaw <quentin.mcgaw@gmail.com>
2019-05-04 11:48:33 +0200

    Restructured server handlers in server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { Loader } from './loader';
 import { logger, debugLog } from './logging';
 import { Pool } from './pool';
 import { CacheHTML } from './cache';
+import { Request, Response } from 'express-serve-static-core';
 
 export class Server {
     app: Express;
@@ -13,48 +14,50 @@ export class Server {
     constructor(port: number, pool: Pool, cache: CacheHTML, timeout: number) {
         this.app = express();
         this.loader = new Loader(pool, cache, timeout);
-        this.setupRoutes(this.app, timeout);
+        this.app.get('/', async (req, res, _) => {
+            this.getRootHandler(req, res)
+        });
+        this.app.get('/healthcheck', async (req, res, _) => {
+            this.getHealthcheckHandler(req, res)
+        });
         this.server = this.app.listen(
             port,
             () => logger.info("server listening on port " + port),
         );
     }
-    setupRoutes(app: Express, timeout: number) {
-        debugLog.server("setting up server routes");
-        app.get('/', async (req, res, _) => {
-            logger.info("received HTTP GET: " + req.url);
-            let url: string;
-            try {
-                url = this.verifyURL(req.query["url"])
-            } catch (e) {
-                return res.status(403).send({
-                    "error": String(e)
-                });
-            }
-            const wait = req.query["wait"];
-            try {
-                const html = await this.loader.spitHTML(url, wait);
-                return res.status(200).send({
-                    "html": html
-                });
-            } catch (e) {
-                logger.error(String(e));
-                return res.status(403).send({
-                    "error": String(e)
-                });
-            }
-        });
-        app.get('/healthcheck', async (req, res, next) => {
-            debugLog.server("received GET /healthcheck request: " + req.url);
-            const healthy = true; // TODO
-            if (healthy) {
-                return res.status(200);
-            }
-            logger.warn("unhealthy");
-            return res.status(500).send("unhealthy");
-        });
+    async getRootHandler(req: Request, res: Response) {
+        logger.info("received HTTP GET: " + req.url);
+        let url: string;
+        try {
+            url = Server.verifyURL(req.query["url"])
+        } catch (e) {
+            return res.status(403).send({
+                "error": String(e)
+            });
+        }
+        const wait = req.query["wait"];
+        try {
+            const html = await this.loader.spitHTML(url, wait);
+            return res.status(200).send({
+                "html": html
+            });
+        } catch (e) {
+            logger.error(String(e));
+            return res.status(403).send({
+                "error": String(e)
+            });
+        }
     }
-    verifyURL(url: string | undefined): string {
+    async getHealthcheckHandler(req: Request, res: Response) {
+        debugLog.server("received GET /healthcheck request: " + req.url);
+        const healthy = true; // TODO
+        if (healthy) {
+            return res.status(200);
+        }
+        logger.warn("unhealthy");
+        return res.status(500).send("unhealthy");
+    }
+    static verifyURL(url: string | undefined): string {
         if (url === undefined || url === "") {
             throw new Error("url parameter not provided")
         } else if (validUrl.isWebUri(url) === undefined) {
